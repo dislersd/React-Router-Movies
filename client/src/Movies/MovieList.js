e2213e9877021265eac6785a1517843321cda78d
dislersd <dislersd@gmail.com>
2019-02-25 15:08:44 -0800

    Stretch Goal: removed repetative code and refactored into moviecard component

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import MovieCard from './MovieCard';
 import axios from "axios";
 
 export default class MovieList extends Component {
@@ -25,31 +26,10 @@ export default class MovieList extends Component {
     return (
       <div className="movie-list">
         {this.state.movies.map(movie => (
-          <Link className='link' to={`/movies/${movie.id}`} key={`${movie.id}`}> <MovieDetails movie={movie} /> </Link>
+          <Link className='link' to={`/movies/${movie.id}`} key={`${movie.id}`}> <MovieCard movie={movie} /> </Link>
         ))}
       </div>
     );
   }
 }
 
-function MovieDetails({ movie }) {
-  const { title, director, metascore, stars } = movie;
-  return (
-    <div className="movie-card">
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-      <h3>Actors</h3>
-
-      {stars.map(star => (
-        <div key={star} className="movie-star">
-          {star}
-        </div>
-      ))}
-    </div>
-  );
-}
