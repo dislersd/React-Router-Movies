a4cb09a5840e75e13a4dca9ca8dd303c82b1fe0d
dislersd <dislersd@gmail.com>
2019-02-26 14:01:40 -0800

    turned saved movies into links

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,6 @@ export default class App extends Component {
   
 
   addToSavedList = movie => {
-    console.log(movie);
     const savedList = this.state.savedList;
     savedList.push(movie);
     this.setState({ savedList });
